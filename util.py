307145a64f580240dd93d03ec72483e21bc321fe
han-keong <hk997@live.com>
2021-07-07 04:31:24 +0800

    fix util

--- util.py
+++ util.py
@@ -1,12 +1,14 @@
 import pandas as pd
-from doc import Document
+from doctypes import Document
+
 
 def row_to_doc_adapter(row: pd.Series) -> Document:
-    return Document(id = row.ID,
-                    title = row.title,
-                    content = row.content)
+    return Document(id=row.ID,
+                    title=row.title,
+                    content=row.content)
 
-def read_docs(docs_path: str, docs_type: str):
+
+def read_docs(docs_path: str, docs_type: str) -> list[Document]:
     reader = getattr(pd, f'read_{docs_type}')
     df = reader(docs_path)
     docs = df.apply(row_to_doc_adapter, axis=1)
