1663ddf046bb9283b040f4189cfc46aeb3283e18
han-keong <hk997@live.com>
2021-07-07 19:03:18 +0800

    fix ranker

--- ranker.py
+++ ranker.py
@@ -2,70 +2,64 @@ from doctypes import TokenizedDocument
 from math import log
 
 
-def tf_(doc: TokenizedDocument, words: list[str]) -> dict[str, int]:
+def calculate_tf(doc: TokenizedDocument) -> dict[int, int]:
     """Calculate term frequency for all words in a document.
 
     Argument:
         - doc: tokenized document
-        - words: word list
     Returns:
         - dict of words to the number of times they occur in the document
     """
     tf = {}
-    for idx in doc.title + doc.content:
-        word = words[idx]
-        if word not in tf:
-            tf[word] = 0
-        tf[word] += 1
+    for i in doc.title + doc.content:
+        if i not in tf:
+            tf[i] = 0
+        tf[i] += 1
     return tf
 
 
-def test_tf_():
-    input_ = (TokenizedDocument(id=0, title=[0, 1, 2, 3], content=[]),
-              ['a', 'b', 'c', 'd'])
-    expected = {'a': 1, 'b': 1, 'c': 1, 'd': 1}
-    output = tf_(*input_)
+def test_calculate_tf():
+    input_ = TokenizedDocument(id=0, title=[0, 1, 2, 3], content=[])
+    expected = {0: 1, 1: 1, 2: 1, 3: 1}
+    output = calculate_tf(input_)
     assert output == expected, 'expected ' + str(
-        expected) + ' from tf_ but got ' + str(output)
+        expected) + ' from calculate_tf but got ' + str(output)
 
 
-test_tf_()
+test_calculate_tf()
 
 
-def df_(docs: list[TokenizedDocument], words: list[str]) -> dict[str, int]:
+def calculate_df(docs: list[TokenizedDocument]) -> dict[int, int]:
     """Calculate document frequency for all words in all documents.
 
     Argument:
         - docs: list of tokenized documents
-        - words: word list
     Returns:
         - dict of words to the number of documents they appear in
     """
     df = {}
     for doc in docs:
-        for idx in set(doc.title + doc.content):
-            word = words[idx]
-            if word not in df:
-                df[word] = 0
-            df[word] += 1
+        for i in set(doc.title + doc.content):
+            if i not in df:
+                df[i] = 0
+            df[i] += 1
     return df
 
 
-def test_df_():
-    input_ = ([TokenizedDocument(id=0, title=[0, 1, 2], content=[]),
-               TokenizedDocument(id=1, title=[1, 2, 3], content=[]),
-               TokenizedDocument(id=2, title=[2, 3, 4], content=[])],
-              ['a', 'b', 'c', 'd', 'e'])
-    expected = {'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 1}
-    output = df_(*input_)
+def test_calculate_df():
+    input_ = [TokenizedDocument(id=0, title=[0, 1, 2], content=[]),
+              TokenizedDocument(id=1, title=[1, 2, 3], content=[]),
+              TokenizedDocument(id=2, title=[2, 3, 4], content=[])]
+    expected = {0: 1, 1: 2, 2: 3, 3: 2, 4: 1}
+    output = calculate_df(input_)
     assert output == expected, 'expected ' + str(
-        expected) + ' from df_ but got ' + str(output)
+        expected) + ' from calculate_df but got ' + str(output)
 
 
-test_df_()
+test_calculate_df()
 
 
-def idf_(df: dict[str, int], corpus_size: int) -> dict[str, float]:
+def calculate_idf(df: dict[int, int], corpus_size: int) -> dict[int, float]:
     """Calculate inverse document frequency.
 
     Arguments:
@@ -75,71 +69,79 @@ def idf_(df: dict[str, int], corpus_size: int) -> dict[str, float]:
         - dict of words to their inverse document frequencies
     """
     idf = {}
-    for word, freq in df.items():
-        idf[word] = round(log(corpus_size / freq), 2)
+    for i, freq in df.items():
+        idf[i] = log(corpus_size / freq)
     return idf
 
 
-def test_idf_():
-    input_ = ({'a': 1, 'b': 2, 'c': 3, 'd': 2, 'e': 1}, 3)
-    expected = {'a': 1.1, 'b': 0.41, 'c': 0.0, 'd': 0.41, 'e': 1.1}
-    output = idf_(*input_)
+def test_calculate_idf():
+    input_ = ({0: 1, 1: 2, 2: 3, 3: 2, 4: 1}, 3)
+    expected = {0: 1.0986122886681098,
+                1: 0.4054651081081644,
+                2: 0.0,
+                3: 0.4054651081081644,
+                4: 1.0986122886681098}
+    output = calculate_idf(*input_)
     assert output == expected, 'expected ' + str(
-        expected) + ' from idf_ but got ' + str(output)
+        expected) + ' from calculate_idf but got ' + str(output)
 
 
-test_idf_()
+test_calculate_idf()
 
 
-def bm25_(
+def calculate_bm25(
         query: list[int],
-        doc: TokenizedDocument,
         docs: list[TokenizedDocument],
-        words: list[str],
+        topk: int = 10,
         k1: float = 1.2,
-        b: float = 0.75) -> float:
-    """Calculate the BM25 retrieval status value for a query and document.
+        b: float = 0.75) -> list[tuple]:
+    """Return the ids of the top k documents with the highest BM25 retrieval status value for the query.
 
     Arguments:
         - query: tokenized query
-        - doc: tokenized document
         - docs: list of tokenized documents which contain a word in the query
-        - words: word list
+        - topk: number of results to return
         - k1: document frequency scaling factor
         - b: document length scaling factor
     Returns:
-        - BM25 retrieval status value for the query and document
+        - list of ids of the top k documents
     """
-    rsv_d = 0.0
     N = len(docs)
-    L_d = len(doc.title + doc.content)
     L_ave = sum(len(doc.title + doc.content)
-                for doc in docs) / N  # calculate average document length
-    tf_d = tf_(doc, words)
-    df = df_(docs, words)
-    idf = idf_(df, N)
-    for t in query:
-        if t not in tf_d:
-            continue
-        idf_t = idf[t]
-        tf_td = tf_d[t]
-        rsv_td = idf_t * (((k1 + 1) * tf_td) /
-                          ((k1 * ((1 - b) + b * (L_d / L_ave)) + tf_td)))
-        rsv_d += rsv_td
-    return round(rsv_d, 2)
+                for doc in docs) / N
+    df = calculate_df(docs)
+    idf = calculate_idf(df, N)
+
+    rsv = {}  # maps document id to score
+    for doc in docs:
+        L_d = len(doc.title + doc.content)
+        rsv_d = 0.0
+        tf_d = calculate_tf(doc)
+        for i in query:
+            if i not in tf_d:
+                continue
+            idf_t = idf[i]
+            tf_td = tf_d[i]
+            rsv_td = idf_t * (((k1 + 1) * tf_td) /
+                              ((k1 * ((1 - b) + b * (L_d / L_ave)) + tf_td)))
+            rsv_d += rsv_td
+        rsv[doc.id] = rsv_d
+
+    rank = sorted(rsv.items(), key=lambda kv: kv[1], reverse=True)
+    return rank[:topk]
 
 
-def test_bm25_():
+def test_calculate_bm25():
     input_ = ([1, 2, 4],
-              TokenizedDocument(id=1, title=[1, 2, 3], content=[]),
               [TokenizedDocument(id=0, title=[0, 1, 2], content=[]),
                TokenizedDocument(id=1, title=[1, 2, 3], content=[]),
-               TokenizedDocument(id=2, title=[2, 3, 4], content=[])],
-              ['a', 'b', 'c', 'd', 'e'])
-    expected = 0.41
-    output = bm25_(*input_)
+               TokenizedDocument(id=2, title=[2, 3, 4], content=[])])
+    expected = [(2, 1.0986122886681098),
+                (0, 0.4054651081081644),
+                (1, 0.4054651081081644)]
+    output = calculate_bm25(*input_)
     assert output == expected, 'expected ' + str(
-        expected) + ' from bm25_ but got ' + str(output)
+        expected) + ' from calculate_bm25 but got ' + str(output)
 
 
-test_bm25_()
+test_calculate_bm25()
