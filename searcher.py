022edb0f3c44e7a903656e248d796059bed551e6
han-keong <hk997@live.com>
2021-07-08 14:00:34 +0800

    fix searcher

--- searcher.py
+++ searcher.py
@@ -1,11 +1,11 @@
+from time import time_ns
 from doctypes import Document
-from util import clean_words, get_doc
 from ranker import calculate_bm25
-import nltk
+from util import clean_words, get_doc
 import json
 
 
-def search(query: str, topk: int = 10, index_path: str = "test.json") -> list[int]:
+def search(query: str, topk: int, index_path: str) -> list[int]:
     """Return the ids of the top k documents for the query.
 
     Arguments:
@@ -18,7 +18,6 @@ def search(query: str, topk: int = 10, index_path: str = "test.json") -> list[in
     # Load the stored indexer output
     with open(index_path, 'r') as fp:
         [docs, words, wtoi, index] = json.load(fp)
-    
     docs = [Document.from_dict(doc) for doc in docs]
     docs_map = {doc.id: doc for doc in docs}
 
@@ -27,32 +26,53 @@ def search(query: str, topk: int = 10, index_path: str = "test.json") -> list[in
     if not query:
         return []
 
-    # Use the inverted index to retrieve the ids of all documents that contain a word in the query
+    # Use the inverted index to retrieve the ids of all documents that contain
+    # a word in the query
     docs_ = list(set(docid for word in query for docid in index[word]))
 
     # Get the tokenized documents with the ids
     for i, docid in enumerate(docs_):
         docs_[i] = docs_map[docid]
-    
+
+    # Convert query to word indices
     query = [wtoi[word] for word in query]
 
     # Call the BM25 algorithm and return the results
     results = calculate_bm25(query, docs_, topk)
 
+    # Select docid
     if results:
-        results = [tup[0] for tup in results]
-    
+        results = [docid for (docid, _) in results]
+
     return results
 
 
 def test_search():
-    input_ = ("Assault On Congress Building", 3)
-    expected = [325084]
-    output = search(*input_)
-    for docid in output:
-        print(get_doc(docid, "test_data.csv", "csv"), "\n")
-    assert output == expected, 'expected ' + str(
-        expected) + ' from search but got ' + str(output)
+    import pandas as pd
+    import time
+
+    total_ms = 0
+    n = 0
+
+    for expected, query in pd.read_csv(
+            "test_queries.csv").itertuples(index=False):
+        print(f"{n+1:2}. {query=}, {expected=}")
+        input_ = (query, 3, "test_index.json")
+        start = time.time()
+        output = search(*input_)
+        end = time.time()
+        time_taken = (end - start) * 1000
+
+        for i, docid in enumerate(output):
+            doc = get_doc(docid, "test_data.csv", "csv")
+            print(f"   {i+1:2}) {doc.id=:7}, {doc.title=}")
+        print(f"    {time_taken=:.0f}ms\n")
+        total_ms += time_taken
+        n += 1
+
+    avg_time_taken = total_ms / n
+    latency = 1000 / avg_time_taken
+    print(f"in {n} queries: {avg_time_taken=:.0f}ms, {latency=:.2f} queries/s")
 
 
 test_search()
