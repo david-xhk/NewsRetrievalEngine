b8f687064aef3e6ed2039cda50f2fb56c038f719
han-keong <hk997@live.com>
2021-07-08 14:00:27 +0800

    fix util

--- util.py
+++ util.py
@@ -37,27 +37,36 @@ def row_to_doc_adapter(row: pd.Series) -> Document:
                     content=row.content)
 
 
-def read_docs(docs_path: str, docs_type: str) -> list[Document]:
+def get_docs(docs_path: str, docs_type: str) -> pd.DataFrame:
     reader = getattr(pd, f'read_{docs_type}')
     df = reader(docs_path)
+    return df
+
+
+def read_docs(docs_path: str, docs_type: str) -> list[Document]:
+    df = get_docs(docs_path, docs_type)
     docs = df.apply(row_to_doc_adapter, axis=1)
     return docs.tolist()
 
 
-def get_doc(id: int, docs_path: str, docs_type: str) -> Document:
-    reader = getattr(pd, f'read_{docs_type}')
-    df = reader(docs_path)
-    res = df[df['ID'] == id]
+def get_doc(docid: int, docs_path: str, docs_type: str) -> Document:
+    df = get_docs(docs_path, docs_type)
+    res = df[df['ID'] == docid]
     if not res.empty:
         return row_to_doc_adapter(res.iloc[0])
 
 
 def test_get_doc():
-    input_ = (318094, 'test_data.csv', 'csv')
-    expected = 318094
+    input_ = (315201, 'test_data.csv', 'csv')
+    expected = 315201
     output = get_doc(*input_)
     assert output.id == expected, 'expected ' + str(
         expected) + ' from get_doc but got ' + str(output)
 
 
 test_get_doc()
+
+
+def get_docs_size(docs_path: str, docs_type: str) -> int:
+    df = get_docs(docs_path, docs_type)
+    return df.memory_usage(deep=True).sum()
